4e139d8d6ab2e9ab586f228031b900ba5f248875
Andreea Oprisor <Andreea.Oprisor1@e.ujf-grenoble.fr>
2015-03-06 14:48:20 +0100

    DAO -> Added methods in ConsulterEpreuve

--- src/java/CyberComp_G2/DAO/ConsituerEquipe/GetConsulterEquipeDAO.java
+++ src/java/CyberComp_G2/DAO/ConsituerEquipe/GetConsulterEquipeDAO.java
@@ -22,10 +22,50 @@ import javax.sql.rowset.CachedRowSet;
  */
 public class GetConsulterEquipeDAO {
     
-    public static final String lesDelegations ="SELECT pays FROM viewDelegation";
+    public static final String lesDelegations = "SELECT * FROM viewDelegation";
     
     public static CachedRowSet getDelegations() throws SQLException{
-        return getConsulterEquipe(lesDelegations,0);
+        return getConsulterEquipe(lesDelegations,"");
+    }
+    
+    public static final String lesEquipes = "SELECT * FROM LesEquipes WHERE pays ='%s'";
+    
+    public static CachedRowSet getEquipes(String pays) throws SQLException{
+        return getConsulterEquipe(lesEquipes, pays);
+    }
+    
+    public static final String lesSportifsDUneEquipe = "SELECT * FROM LesSportifs WHERE idSportif = %d";
+    
+    /**
+     *
+     * @param idEquipe
+     * @return
+     * @throws SQLException
+     */
+    
+    public static CachedRowSet getSportifsDUneEquipe(int idEquipe) throws SQLException{
+        return getConsulterEquipe(lesSportifsDUneEquipe, idEquipe);
+    }
+    
+    public static final String lesSportifs = "SELECT * FROM LesSportifs";
+    
+    /**
+     *
+     * @param idSportif
+     * @return
+     * @throws SQLException
+     */
+    public static CachedRowSet getConsulterEquipe(int idSportif) throws SQLException{
+        return getConsulterEquipe(lesSportifs, idSportif);
+    } 
+    
+    private static CachedRowSet getConsulterEquipe(String query, String selecteur) 
+            throws SQLException {
+        CachedRowSet crs = new CachedRowSetImpl();
+        crs.setDataSourceName("java:comp/env/jdbc/BDCyberCompetition");
+        crs.setCommand(String.format(query,selecteur));
+        crs.execute();
+        return crs;
     }
     
     private static CachedRowSet getConsulterEquipe(String query, int selecteur) 
